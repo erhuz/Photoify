199239db54f2c636608ff9bec1d3ff2c0fa78537
Benjamin Fransson <benjamin@cafcon.se>
2018-12-16 14:07:01 +0100

    Added PostsController to handle post creation, update & delete

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function showNewPostForm()
+    {
+        return view('posts.new');
+    }
+
+    public function create(array $data)
+    {
+        dd($data);
+    }
+}
+
