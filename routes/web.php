58708a790c25fa556885c5b7224a02cb5367d887
Benjamin Fransson <benjamin@cafcon.se>
2018-12-18 00:46:56 +0100

    Removed duplicate name

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Auth::routes(); // This bit of code corresponds with all commented routes above
 Route::get('/home', 'HomeController@index')->name('home');
 
 // User routes
-Route::get('user', 'UserController@profile')->name('profile');
+Route::get('user', 'UserController@profile');
 Route::get('user/profile', 'UserController@profile')->name('profile');
 
 // Users routes
