f059863b5f7d1e25c03613e55348d0537c9f17dc
Benjamin Fransson <benjamin@cafcon.se>
2018-12-18 03:21:59 +0100

    Added conditional statement to check if javascript should run on page

--- resources/js/custom/profile-img.js
+++ resources/js/custom/profile-img.js
@@ -1,26 +1,30 @@
-const clickableText          = document.querySelector('.user-profile .img-change');
-const profileImgContainerDiv = document.querySelector('.user-profile .profile-img-container > div');
-const profileImgForm         = document.querySelector('.user-profile .profile-img-form');
-let   profileImgInput        = document.querySelector('.user-profile #image');
-const profileImg             = document.querySelector('.profile-img-container img');
-
 (function(){
-  clickableText.addEventListener("click", () => openProfileImgForm());
-  profileImgContainerDiv.addEventListener("click", () => openProfileImgForm());
+  if(document.querySelector('.user-profile')){
+    const clickableText          = document.querySelector('.user-profile .img-change');
+    const profileImgContainerDiv = document.querySelector('.user-profile .profile-img-container > div');
+    const profileImgForm         = document.querySelector('.user-profile .profile-img-form');
+    let   profileImgInput        = document.querySelector('.user-profile #image');
+    const profileImg             = document.querySelector('.profile-img-container img');
 
-  function openProfileImgForm(){
-    profileImgInput.click();
-  };
 
-  profileImgForm.addEventListener("change", () => {
-    if(profileImgInput.files && profileImgInput.files[0]){
-      profileImg.setAttribute('src',
-      window.URL.createObjectURL(profileImgInput.files[0])
-      );
-    }
-    console.log(profileImgInput.files[0]);
-    console.log('Send request to api');
-  });
+    clickableText.addEventListener("click", () => openProfileImgForm());
+    profileImgContainerDiv.addEventListener("click", () => openProfileImgForm());
 
-})();
+    function openProfileImgForm(){
+      profileImgInput.click();
+    };
+
+    profileImgForm.addEventListener("change", () => {
+      if(profileImgInput.files && profileImgInput.files[0]){
+        profileImg.setAttribute('src',
+        window.URL.createObjectURL(profileImgInput.files[0])
+        );
+      }
+      console.log(profileImgInput.files[0]);
+      console.log('Send request to api');
+    });
+
+  }else{
+    console.log('Not user profile page.');
+  }})();
 
