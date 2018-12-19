751faa21415db24dc925eb22226fbfa6b5e112b3
Benjamin Fransson <benjamin@cafcon.se>
2018-12-19 22:29:59 +0100

    Added avatar to fillable

--- app/User.php
+++ app/User.php
@@ -1,10 +1,9 @@
-<?php declare(strict_types=1);
+<?php declare (strict_types = 1);
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -16,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'avatar',
     ];
 
     /**
