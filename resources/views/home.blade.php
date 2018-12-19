52c1e507f6f4552f1629c80990d58a18d049f12d
Benjamin Fransson <benjamin@cafcon.se>
2018-12-19 02:57:48 +0100

    Added placeholder for a regular post feed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,59 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row justify-content-center post-card-container">
+
+        <div class="col-md-10 col-lg-4 side-menu">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+
+                <a href="{{ route('profile') }}">
+                    <div class="card-header">
+                        <div class="profile-picture-container">
+                            <img class="profile-picture rounded-circle" src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" alt="{{ Auth::user()->name }}">
+                        </div>
+                        <h5>{{ Auth::user()->name }}</h5>
+                    </div>
+                </a>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <a class="btn btn-primary btn-block" href="{{ route('my posts') }}">My posts</a>
+                    <a class="btn btn-primary btn-block" href="{{ route('new post') }}">New post</a>
+                    <a class="btn btn-primary btn-block" href="#">Something else</a>
                 </div>
+
+            </div>
+        </div>
+
+        <div class="col-md-10 col-lg-6">
+            @for ($i = 1; $i < 20; $i++)
+            <div class="row post">
+                <div class="col">
+                    <div class="card">
+
+                        <div class="card-header">
+                            <div class="profile-picture-container">
+                                <img class="profile-picture rounded-circle" src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" alt="">
+                            </div>
+                            <h5 class="card-title"><a href="{{__('User link')}}">{{__('Username')}}</a></h5>
+                        </div>
+
+                        <a class="card-image" href="#{{__('Post page')}}">
+                            <img class="" src="https://via.placeholder.com/{{mt_rand(5,20) * 100}}x{{mt_rand(5,20) * 100}}"
+                            alt="Card image cap">
+                        </a>
+
+                        <div class="card-body">
+                            <div class="card-text">
+                                <p>Some quick example text to build on the card title and make up the bulk of the
+                                    card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                @endfor
             </div>
         </div>
     </div>
-</div>
-@endsection
+    @endsection
