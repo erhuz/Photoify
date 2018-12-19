706a374576567888f1c9119760a0ecfd4bf844bb
Benjamin Fransson <benjamin@cafcon.se>
2018-12-19 22:30:52 +0100

    PHP CS Fixer

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,8 +1,9 @@
-<?php declare(strict_types=1);
+<?php
+
+declare (strict_types = 1);
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Post;
 
 class HomeController extends Controller
