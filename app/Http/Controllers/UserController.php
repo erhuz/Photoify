be19473c742414f3e68633710e03ede4b6943ffd
Benjamin Fransson <benjamin@cafcon.se>
2018-12-19 22:31:34 +0100

    Added setAvatar function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,8 +1,12 @@
-<?php declare(strict_types=1);
+<?php
+
+declare (strict_types = 1);
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -17,12 +21,26 @@ class UserController extends Controller
     }
 
     /**
-     * Show the current users profile
+     * Show the current users profile.
      *
      * @return \Illuminate\Http\Response
      */
     public function profile()
     {
         return view('user.profile');
+    }
+
+    public function setAvatar(Request $request)
+    {
+        $validatedData = $request->validate([
+            'avatar' => 'required|mimes:jpeg,gif,png',
+        ]);
+
+        $avatar_path = $validatedData['avatar']->storeAs('public/avatars', $request->user()->id);
+
+        Auth::user()->avatar = $avatar_path;
+        Auth::user()->save();
+
+        return back();
     }
 }
